c7fac30322b11c0017b9cc3cfa4e01bddc62807f
Vaim Dev <dev@v-aim.com>
2025-01-30 23:32:49 +0800

    feat(scraper): sort teams by average player rank in leaderboard output

--- python/dota_teams.py
+++ python/dota_teams.py
@@ -20,10 +20,19 @@ def group_players_by_team_id(json_file_path: str) -> dict:
 # Example usage
 if __name__ == "__main__":
     regions = ["europe", "americas", "se_asia", "china"]
-    json_file_path = f"json/{regions[3]}_leaderboard.json"
+    json_file_path = f"json/{regions[0]}_leaderboard.json"
     teams = group_players_by_team_id(json_file_path)
-    for team_id, players in teams.items():
+
+    sorted_teams = sorted(
+        teams.items(),
+        key=lambda item: sum(player["rank"] for player in item[1]) / len(item[1]),
+    )
+
+    for team_id, players in sorted_teams:
         if len(players) > 3:
             print(f"Team ID: {team_id}")
+            print(
+                f"Average rank: {sum(player['rank'] for player in players) / len(players)}"
+            )
             for player in players:
                 print(player)
